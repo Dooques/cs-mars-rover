edf706bba94005508a20f4472f1e2751a168403e
Sam <samuelgdukes@gmail.com>
2026-04-13 10:48:42 +0100

    Create InstructionSet class

---
+++ MarsRoverProject/InstructionSet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    internal record InstructionSet()
+    {
+        private static readonly List<InstructionSet> InstructionSetList = [];
+        Instruction InstructionCommand;
+        Direction Direction;
+
+        public InstructionSet(Instruction instructionCommand, Direction direction) : this()
+        {
+            this.InstructionCommand = instructionCommand;
+            this.Direction = direction;
+            InstructionSetList.Add(this);
+        }
+
+        public List<InstructionSet> GetListOfInstructions()
+        {
+            return InstructionSetList;
+        }
+
+    }
+}
