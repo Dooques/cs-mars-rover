d8125b99ad5dccd623dc4828380ebbfd93689331
Dooques <samuelgdukes@gmail.com>
2026-04-13 20:20:43 +0100

    Testing position input parser, all tests passing

--- MarsRoverProject/Input Layer/Input Parsing/InputParser.cs
+++ MarsRoverProject/Input Layer/Input Parsing/InputParser.cs
@@ -55,14 +55,57 @@ namespace MarsRover
                 } else if (x > 0 && y > 0)
                 {
                     break;
-                }            }
+                }            
+            }
             dm.CreatePlateau(x, y);
             return dm.GetPlateau();
         }
 
-        public Position ParsePositionInput()
+        public Position ParsePositionInput(string userInput)
         {
-            return new Position(0, 0, Direction.North);
+            var dm = new DataManager();
+            int x = 0;
+            int y = 0;
+            Direction direction = Direction.None;
+            foreach (var i in userInput.ToCharArray())
+            {
+                if (x == 0)
+                {
+                    Console.WriteLine(i);
+                    Int32.TryParse(i.ToString(), out x);
+                    continue;
+                }
+                else if (y == 0)
+                {
+                    Console.WriteLine(i);
+                    Int32.TryParse(i.ToString(), out y);
+                    continue;
+                }
+                else if (direction is Direction.None)
+                {
+                    var directionNormalised = i switch
+                    {
+                        'N' => "North",
+                        'S' => "South",
+                        'E' => "East",
+                        'W' => "West",
+                        _ => "Skip"
+                    };
+                    if (directionNormalised is "Skip")
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(i);
+                    Enum.TryParse(directionNormalised, out direction);
+                }           
+            }
+            if (direction is Direction.None)
+            {
+                x = 0;
+                y = 0;
+            }
+            dm.CreateRover(x, y, direction);
+            return dm.GetRoverPosition();
         }
     }
 }
--- Tests/PositionParsingTests.cs
+++ Tests/PositionParsingTests.cs
@@ -10,10 +10,20 @@ namespace Tests
     internal class PositionParsingTests
     {
         [TestCase("1 1 N", 1, 1, Direction.North)]
+        [TestCase("2 2 S", 2, 2, Direction.South)]
+        [TestCase("1 4 E", 1, 4, Direction.East)]
+        [TestCase("6 2 W ", 6, 2, Direction.West)]
+        [TestCase("", 0, 0, Direction.None)]
+        [TestCase("hello", 0, 0, Direction.None)]
+        [TestCase("123456789", 0, 0, Direction.None)]
         public void ParsingPosition_ReturnPosition_VariousValues(string input, int expectedX, int expectedY, Direction expectedDirection)
         {
             var ip = new InputParser();
             var testPosition = ip.ParsePositionInput(input);
+
+            Assert.That(testPosition.X, Is.EqualTo(expectedX));
+            Assert.That(testPosition.Y, Is.EqualTo(expectedY));
+            Assert.That(testPosition.Direction, Is.EqualTo(expectedDirection));
         }
     }
 }
